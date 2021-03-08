38545d6f4fa27465cbf32cba6066e2db56aeece8
jaluik <cqzhengmouren@gmail.com>
2021-03-08 23:33:54 +0800

    feat: 优化首页的样式

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,8 @@
 import React from 'react'
 import clsx from 'clsx'
 import Layout from '@theme/Layout'
-import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
-import useBaseUrl from '@docusaurus/useBaseUrl'
 import styles from './styles.module.css'
-
-const features = [
-  {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-]
-
-function Feature({ imageUrl, title, description }) {
-  const imgUrl = useBaseUrl(imageUrl)
-  return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className='text--center'>
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  )
-}
 
 function Home() {
   const context = useDocusaurusContext()
@@ -63,10 +13,10 @@ function Home() {
       description='jaluik的个人博客，包含前端、后端、运维相关的内容。'>
       <main className={styles.wrap}>
         <div className={styles.main}>
-          <div>
-            <h1 className={styles.content}>jaluik</h1>
-            <div className={styles.content}>think more</div>
-            <div className={styles.content}>do better</div>
+          <div className={styles.pannel}>
+            <button className={styles.btn}>I AM JALUIK</button>
+            <h2 className={styles.content}>think more</h2>
+            <h2 className={styles.content}>do better</h2>
           </div>
           <img src='img/zoro.png' className={styles.bg} />
         </div>
