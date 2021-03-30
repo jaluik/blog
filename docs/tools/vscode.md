04f80ba2685425f4ee06e366c9d3f3a0f8a1eb1a
jaluik <cqzhengmouren@gmail.com>
2021-03-30 20:41:10 +0800

    feat: TODO vscode官方文档

---
+++ docs/tools/vscode.md
@@ -0,0 +1,14 @@
+---
+id: vscode
+title: vscode实用技巧
+sidebar_label: vscode实用技巧
+description: 记录vscode官方文档中的一些实用技巧
+keywords:
+  - vscode
+  - 实用技巧
+slug: /tools/vscode
+---
+
+> 这篇文章用来记录查看 vscode 官方文档时做的总结
+
+## vscode 实用快捷键
--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   firstSidebar: {
     ['源码学习']: ['sourceCode/redux'],
+    ['工具集']: ['tools/vscode'],
     // Features: ['mdx'],
   },
   secondSidebar: ['server/gitActionsDeploy', 'server/pythonProxyPool'],
